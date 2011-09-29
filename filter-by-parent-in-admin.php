c42963df3287b5faf93d55c9bf47680fdb35f857
irvingswiftj <irvingswiftj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-29 16:14:23 +0000

    committing version 1 to trunk
    git-svn-id: https://plugins.svn.wordpress.org/filter-pages-by-parent-in-admin/trunk@445302 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ filter-by-parent-in-admin.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Filter by parent in admin
+Plugin URI: http://www.electricstudio.co.uk
+Description: Filter pages in wp-admin by their parent page
+Version: 1.0
+Author: James Irving-Swift
+Author URI: http://www.irving-swift.com
+License: GPL2
+*/
+
+add_action('restrict_manage_posts','filter_by_parent_in_admin');
+add_filter('parse_query','filter_the_pages');
+
+function filter_by_parent_in_admin(){
+    echo "Show only children of: ";
+    $dropdown_options = array(
+        'show_option_none' => __( ' - ' ),
+        'depth' => 2,
+        'hierarchical' => 1,
+        'post_type' => 'page',
+        'sort_column' => 'name',
+        'selected' => $_GET['parentId'],
+        'name' => 'parentId'
+    );
+
+    wp_dropdown_pages( $dropdown_options );   
+}
+
+function filter_the_pages($query) {
+    $childPages = get_pages(array('child_of' => $_GET['parentId']));
+    
+    $filteredPages = array($_GET['parentId']);
+    
+    foreach($childPages as $cp){
+    	array_push($filteredPages, $cp->ID);
+    }
+    
+    global $pagenow;
+    $qv = &$query->query_vars;
+    if ($pagenow=='edit.php' && $qv['post_type']=='page') {
+        $qv['post__in'] = $filteredPages;
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Electric Studio Mailer ===
+Contributors: irvingswiftj, Electric Studio
+Tags: wp-admin, parent, filter, pages, page, sort, child
+Requires at least: 3.1
+Tested up to: 3.2.1
+Stable tag: 1.0
+
+Filter pages in the wp-admin by their parent
+
+== Description ==
+
+Adds a filter in your wp-admin that allows you to choose a parent page so that only the children of that page will be shown. Very useful if you have a lot of pages!
+
+
+== Installation ==
+
+Install from wordpress plugins directory.
+
+Else, to install manually:
+
+1. Upload unzipped plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. The filter select box (the right most option box)
+
+== Changelog ==
+
+= 1.0 =
+* Version 1.
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+
