92d5ba1bd256e51c9ea100b6372567436b7a15c9
irvingswiftj <irvingswiftj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-19 15:52:34 +0000

    commiting version trunk version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/filter-pages-by-parent-in-admin/trunk@520922 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- filter-pages-by-parent-in-admin.php
+++ filter-pages-by-parent-in-admin.php
@@ -3,48 +3,59 @@
 Plugin Name: Filter by parent in admin
 Plugin URI: http://www.electricstudio.co.uk
 Description: Filter pages in wp-admin by their parent page
-Version: 1.1
+Version: 1.2
 Author: James Irving-Swift
 Author URI: http://www.irving-swift.com
 License: GPL2
 */
 
-add_action('restrict_manage_posts','filter_by_parent_in_admin');
-add_filter('parse_query','filter_the_pages');
 
-function filter_by_parent_in_admin(){
-    echo "Show only children of: ";
-    $dropdown_options = array(
-        'show_option_none' => __( ' - ' ),
-        'depth' => 2,
-        'hierarchical' => 1,
-        'post_type' => 'page',
-        'sort_column' => 'name',
-        'selected' => $_GET['parentId'],
-        'name' => 'parentId'
-    );
+class ES_page_filter {
 
-    wp_dropdown_pages( $dropdown_options );   
+    public function __construct(){
+        add_action('restrict_manage_posts',array($this, 'filter_by_parent_in_admin'));
+        add_filter('parse_query',array($this,'filter_the_pages'));
+    }
+    
+    public function filter_by_parent_in_admin(){
+        echo "Show only children of: ";
+        $dropdown_options = array(
+            'show_option_none' => __( ' - ' ),
+            'depth' => 2,
+            'hierarchical' => 1,
+            'post_type' => $_GET['post_type'],
+            'sort_column' => 'name',
+            'selected' => $_GET['parentId'],
+            'name' => 'parentId'
+        );
+    
+        wp_dropdown_pages( $dropdown_options );   
+    }
+    
+    public function filter_the_pages($query) {
+        $childPages = get_pages(
+            array(
+                'child_of' => $_GET['parentId'],
+                'post_status' => array('publish','draft','trash')
+                )
+             );
+        
+        $filteredPages = array($_GET['parentId']);
+        
+        foreach($childPages as $cp){
+        	array_push($filteredPages, $cp->ID);
+        }
+        
+        global $pagenow;
+        $qv = &$query->query_vars;
+        if ($pagenow=='edit.php' && $qv['post_type']=='page') {
+            $qv['post__in'] = $filteredPages;
+        }
+    
+    }
+
 }
 
-function filter_the_pages($query) {
-    $childPages = get_pages(
-        array(
-            'child_of' => $_GET['parentId'],
-            'post_status' => array('publish','draft','trash')
-            )
-         );
-    
-    $filteredPages = array($_GET['parentId']);
-    
-    foreach($childPages as $cp){
-    	array_push($filteredPages, $cp->ID);
-    }
-    
-    global $pagenow;
-    $qv = &$query->query_vars;
-    if ($pagenow=='edit.php' && $qv['post_type']=='page') {
-        $qv['post__in'] = $filteredPages;
-    }
-
+if(is_post_type_hierarchical($_GET['post_type'])){
+    $es_page_filter = new ES_page_filter();
 }
