48bbeddc1ead880ca9d10532ad286cc02cd9e568
irvingswiftj <irvingswiftj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-13 14:11:18 +0000

    Commiting version 1.3 to trunk
    git-svn-id: https://plugins.svn.wordpress.org/filter-pages-by-parent-in-admin/trunk@530662 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- filter-pages-by-parent-in-admin.php
+++ filter-pages-by-parent-in-admin.php
@@ -3,7 +3,7 @@
 Plugin Name: Filter by parent in admin
 Plugin URI: http://www.electricstudio.co.uk
 Description: Filter pages in wp-admin by their parent page
-Version: 1.2
+Version: 1.3
 Author: James Irving-Swift
 Author URI: http://www.irving-swift.com
 License: GPL2
@@ -18,43 +18,65 @@ class ES_page_filter {
     }
     
     public function filter_by_parent_in_admin(){
-        echo "Show only children of: ";
-        $dropdown_options = array(
-            'show_option_none' => __( ' - ' ),
-            'depth' => 2,
-            'hierarchical' => 1,
-            'post_type' => $_GET['post_type'],
-            'sort_column' => 'name',
-            'selected' => $_GET['parentId'],
-            'name' => 'parentId'
-        );
-    
-        wp_dropdown_pages( $dropdown_options );   
-    }
+        global $pagenow;
+        
+	    if ($pagenow=='edit.php' && $_GET['post_type']=='page') {
+	        
+	        echo "Show only children of: ";
+	        
+	        if (isset($_GET['parentId'])) {
+		        $dropdown_options = array(
+		            'show_option_none' => __( ' - ' ),
+		            'depth' => 2,
+		            'hierarchical' => 1,
+		            'post_type' => $_GET['post_type'],
+		            'sort_column' => 'name',
+		            'selected' => $_GET['parentId'],
+		            'name' => 'parentId'
+		        );
+	        } else {
+		        $dropdown_options = array(
+		            'show_option_none' => __( ' - ' ),
+		            'depth' => 2,
+		            'hierarchical' => 1,
+		            'post_type' => $_GET['post_type'],
+		            'sort_column' => 'name',
+		            'name' => 'parentId'
+		        );
+	        }
+	        
+	        wp_dropdown_pages( $dropdown_options );   
+        }
+    } //END METHOD filter_by_parent_in_admin
     
     public function filter_the_pages($query) {
-        $childPages = get_pages(
-            array(
-                'child_of' => $_GET['parentId'],
-                'post_status' => array('publish','draft','trash')
-                )
-             );
+    
+        if (isset($_GET['parentId'])) {
+	        global $pagenow;
+    
+	        $childPages = get_pages(
+	            array(
+	                'child_of' => $_GET['parentId'],
+	                'post_status' => array('publish','draft','trash')
+	                )
+	             );
+	        
+	        $filteredPages = array($_GET['parentId']);
+	        
+	        foreach($childPages as $cp){
+	        	array_push($filteredPages, $cp->ID);
+	        }
+	        
+	        $qv = &$query->query_vars;
+	        if ($pagenow=='edit.php' && $qv['post_type']=='page') {
+	            $qv['post__in'] = $filteredPages;
+	        }
         
-        $filteredPages = array($_GET['parentId']);
-        
-        foreach($childPages as $cp){
-        	array_push($filteredPages, $cp->ID);
-        }
-        
-        global $pagenow;
-        $qv = &$query->query_vars;
-        if ($pagenow=='edit.php' && $qv['post_type']=='page') {
-            $qv['post__in'] = $filteredPages;
         }
     
-    }
-
-}
+    } //END METHOD filter_the_pages
+    
+} //END CLASS
 
 if(is_post_type_hierarchical($_GET['post_type'])){
     $es_page_filter = new ES_page_filter();
--- readme.txt
+++ readme.txt
@@ -3,14 +3,14 @@ Contributors: irvingswiftj, Electric Studio
 Tags: wp-admin, parent, filter, pages, page, sort, child
 Requires at least: 3.1
 Tested up to: 3.3.1
-Stable tag: 1.2
+Stable tag: 1.3
 
 Filter pages in the wp-admin by their parent
 
 == Description ==
 
 Adds a filter in your wp-admin that allows you to choose a parent page so that only the children of that page will be shown. Very useful if you have a lot of pages!
-
+Thank you to Brian Krogsgard for the changes in version 1.3 (Twitter: @Krogsgard)
 
 == Installation ==
 
@@ -39,6 +39,10 @@ Else, to install manually:
 * No longer erroneously appear in the media section
 * Is now Object Orientated rather than procedural 
 
+= 1.3 =
+* Filtering improved
+* Dropdown menu is better organised when showing filtered posts
+
 == Upgrade Notice ==
 
 = 1.0 =
@@ -49,3 +53,7 @@ Bug fix
 
 = 1.2 =
 Slight fine tuning
+
+= 1.3 =
+* Filtering improved
+* Dropdown menu is better organised when showing filtered posts
